8d1a4a8115eb0ae346ac45d56c4a65273254a0ed
WeiYi Chen <wych@WeiYis-MacBook-Air.local>
2024-03-10 00:22:37 +0800

    fix client-side issues

--- client.cc
+++ client.cc
@@ -8,15 +8,18 @@ int main(void){
     sf::Socket::Status status = socket.connect("127.0.0.1", 50011);
 
     // send message to the server;
-    std::string message = "Hello, this is from client";
+    std::string message = "Hello server, this message is from client";
     socket.send(message.c_str(), message.size() + 1);
 
 
     // receive the message from the server;
     char buff[1024];
     std::size_t received = 0; // the length of the recevied message;
-    socket.receive(buff, sizeof(buff), received);
-    std::cout << buff << "/" << sizeof(buff) << "/" << received << std::endl;
+    socket.receive(buff, sizeof(buff) - 1, received);
+    buff[received] = '\0';
+    //std::cout << buff << "/" << sizeof(buff) << "/" << received << std::endl;
+    std::cout << buff << std::endl;
 
+    socket.disconnect();
     return 0;
 }
--- server.cc
+++ server.cc
@@ -20,12 +20,15 @@ int main(void){
     // Receive the message from the client;
     char buff[1024];
     std::size_t received = 0; // the length of the recevied message;
-    socket.receive(buff, sizeof(buff), received);
-    std::cout << buff << "/" << sizeof(buff) << "/" << received << std::endl;
+    socket.receive(buff, sizeof(buff) - 1, received);
+    //std::cout << buff << "/" << sizeof(buff) << "/" << received << std::endl;
+    std::cout << buff << std::endl;
 
     // Send back to client
-    std::string message = "Welcome, client";
+    std::string message = "Welcome, client with IP: " + socket.getRemoteAddress().toString() + "!\0";
     socket.send(message.c_str(), message.size() + 1);
+
+    socket.disconnect();
 
     return 0;
 }
